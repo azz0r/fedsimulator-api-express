4d19640fcce34e0c8458e9aa6720e79f1bf11919
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2019-04-20 20:14:16 +0100

    CORS improvement

--- express/server.js
+++ express/server.js
@@ -5,16 +5,28 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 
-const app = express();
-
-app.use(cors());
-
-const router = express.Router();
-
+// import data
 const championships = JSON.parse(fs.readFileSync("championships.json", "utf8"));
 const brands = JSON.parse(fs.readFileSync("brands.json", "utf8"));
 const wrestlers = JSON.parse(fs.readFileSync("wrestlers.json", "utf8"));
 
+// initiate instances
+const app = express();
+const router = express.Router();
+
+// app
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// router
 router.get("/", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.json({ brands, wrestlers, championships });
@@ -33,10 +45,12 @@ router.get("/wrestlers", (req, res) => {
   res.json(wrestlers);
 });
 
+// extra app
 app.use("/", router);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/.netlify/functions/server", router); // path must route to lambda
 
+// export
 module.exports = app;
 module.exports.handler = serverless(app);
