72874a739b0580e82e3280434ad0fd776c2f32cc
Aaron Lote <5314860+azz0r@users.noreply.github.com>
2019-04-20 14:32:29 +0100

    Update router

--- express/server.js
+++ express/server.js
@@ -1,18 +1,38 @@
-'use strict';
-const express = require('express');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const bodyParser = require('body-parser');
 
+const bodyParser = require("body-parser");
+const fs = require("fs");
 const router = express.Router();
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>FedSimulator API V1</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/brands', (req, res) => {
+  const brands = JSON.parse(fs.readFileSync("wrestlers.json", "utf8"));
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(brands);
+});
+router.get('/championships', (req, res) => {
+  const championships = JSON.parse(fs.readFileSync("wrestlers.json", "utf8"));
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(championships);
+});
+router.get('/wrestlers', (req, res) => {
+  const wrestlers = JSON.parse(fs.readFileSync("wrestlers.json", "utf8"));
+
+  res.setHeader("Content-Type", "application/json");
+  res.json(wrestlers);
+});
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+app.use(router);
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
